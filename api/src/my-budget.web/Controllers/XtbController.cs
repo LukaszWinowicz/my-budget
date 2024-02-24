31da3f50974a83c88965cda530916ac93a7788f6
Łukasz Winowicz <lwinowicz@gmail.com>
2024-02-24 12:21:11 +0100

    create folder controller in project my-budget.web, with controller XtbController, created first endpoint Login

---
+++ api/src/my-budget.web/Controllers/XtbController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using xAPI.Commands;
+using xAPI.Responses;
+using xAPI.Sync;
+
+namespace my_budget.web.Controllers
+{
+    [Route("api/xtb")]
+    [ApiController]
+    public class XtbController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult Legin([FromBody] LoginModel loginModel) 
+        {
+            Server serverData = Servers.DEMO;
+
+            string appName = "firstApp";
+            string appId = "";
+
+            try
+            {
+                // Connect to server
+                SyncAPIConnector connector = new SyncAPIConnector(serverData);
+
+                // Login to server
+                Credentials credentials = new Credentials(loginModel.userId, loginModel.password, appId, appName);
+                LoginResponse loginResponse = APICommandFactory.ExecuteLoginCommand(connector, credentials, true);
+
+                // Login Response - StreamSessionId
+                var StreamSessionId = loginResponse.StreamSessionId;
+                return Ok(loginResponse);
+
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized(new { message = "An exception occured: " + ex.ToString() });
+            }
+        }
+
+        public class LoginModel
+        {
+            public long userId { get; set; }
+            public string password { get; set; }
+        }
+    }
+}
