f480c52023245fbd7cabae2e19c2ea679c074968
Łukasz Winowicz <lwinowicz@gmail.com>
2024-02-26 18:40:28 +0100

    working on GetMyAccountValueByStreaming

--- api/src/my-budget.web/Controllers/XtbController.cs
+++ api/src/my-budget.web/Controllers/XtbController.cs
@@ -37,19 +37,39 @@ namespace my_budget.web.Controllers
         }
 
         [HttpGet("GetMyAccountValueByStreaming")]
-        public IActionResult GetMyAccountValueByStreaming()
+        public async Task<IActionResult> GetMyAccountValueByStreaming()
         {
+            var tcs = new TaskCompletionSource<StreamingBalanceRecord>();
+
             try
             {
+                Server serverData = Servers.DEMO;
+                SyncAPIConnector connector = new SyncAPIConnector(serverData);
+                Credentials credentials = new Credentials("1234", "haslo");
+                APICommandFactory.ExecuteLoginCommand(connector, credentials);
+                connector.Streaming.Connect();
 
-                return Ok();
+                // Zmieniona metoda obsługi zdarzeń, aby użyć TaskCompletionSource
+                void handler(StreamingBalanceRecord balanceRecord)
+                {
+                    tcs.SetResult(balanceRecord);
+                    // Opcjonalnie, odłącz handler po otrzymaniu danych
+                    connector.Streaming.BalanceRecordReceived -= handler;
+                }
+
+                connector.Streaming.BalanceRecordReceived += handler;
+                connector.Streaming.SubscribeBalance();
+
+                // Oczekiwanie na otrzymanie danych
+                var balanceRecord = await tcs.Task;
+                return Ok(new { Balance = balanceRecord.ToString() });
             }
             catch (Exception ex)
             {
-
                 return Unauthorized(new { message = "An exception occured: " + ex.ToString() });
             }
         }
+
 
         [HttpGet("GetAllSymbols")]
         public IActionResult GetAllSymbols()
